8ed41ec5d34512526a25d87820abb1e7fdda0f45
Monnylly <monnylly@gmail.com>
2022-01-14 18:47:20 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  const palestrante = fs.readFileSync('./talker.json', 'utf-8');
+  const palestranteArray = JSON.parse(palestrante);
+  if (palestranteArray.length === 0) {
+    return res.status(200).json([]);
+  }
+  res.status(200).json(palestranteArray);
 });
 
 app.listen(PORT, () => {
